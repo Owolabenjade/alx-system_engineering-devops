4979cd8472d0626f47656ae5f9192727c3a11251
Benjamin <owolabenjade@gmail.com>
2024-04-22 16:13:21 -0700

    most high

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Puppet manifest to configure SSH client settings
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
+  replace => true,
+}
+
