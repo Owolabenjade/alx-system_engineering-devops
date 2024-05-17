c3541b25fbc005f85c3f37527787af0e7bab4bbb
Benjamin <owolabenjade@gmail.com>
2024-05-17 09:25:39 -0700

    Tolu

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,65 +1,25 @@
-# Puppet manifest to install and configure Nginx with a 'Hello World!' page and a redirection
+# Setup New Ubuntu server with nginx
 
-class nginx_setup {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    content => template('nginx/default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-# Template content for Nginx site configuration
-define nginx::config {
-  file { "/etc/nginx/sites-enabled/${name}":
-    ensure  => link,
-    target  => "/etc/nginx/sites-available/${name}",
-    require => File["/etc/nginx/sites-available/${name}"],
-    notify  => Service['nginx'],
-  }
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-nginx_setup { 'default': }
-
-# Define the template separately or inline here using 'content'
-file { 'nginx/default.erb':
-  content => '
-server {
-  listen 80;
-  server_name _;
-
-  location /redirect_me {
-    return 301 https://www.example.com;
-  }
-
-  location / {
-    root /var/www/html;
-    index index.html;
-  }
-
-  error_page 404 /404.html;
-  location = /404.html {
-    root /var/www/html;
-    internal;
-  }
-}',
-  before => Nginx::Config['default'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-nginx::config { 'default': }
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
 
