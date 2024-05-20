3645bc172bb2d9a7b36dfb06a9558c5aeece58d7
Benjamin <owolabenjade@gmail.com>
2024-05-20 12:26:33 -0700

    ire

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
+"""
+This module fetches and displays the TODO list progress of a given employee
+from a REST API.
+
+Usage:
+    python3 0-gather_data_from_an_API.py <employee_id>
+
+Arguments:
+    <employee_id> - The ID of the employee whose TODO list progress is to be fetched.
+
+Example:
+    python3 0-gather_data_from_an_API.py 2
+"""
+
 import requests
 import sys
 
 def fetch_employee_data(employee_id):
+    """
+    Fetches employee data from the API.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        dict: A dictionary containing employee data if the request is successful.
+        None: If the request is unsuccessful.
+    """
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     response = requests.get(url)
     if response.status_code != 200:
@@ -10,6 +34,16 @@ def fetch_employee_data(employee_id):
     return response.json()
 
 def fetch_todos(employee_id):
+    """
+    Fetches TODO list data for the given employee ID from the API.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        list: A list of dictionaries containing TODO list data if the request is successful.
+        None: If the request is unsuccessful.
+    """
     url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     response = requests.get(url)
     if response.status_code != 200:
@@ -17,6 +51,10 @@ def fetch_todos(employee_id):
     return response.json()
 
 if __name__ == "__main__":
+    """
+    Main entry point of the script. Fetches and prints the TODO list progress of an employee
+    based on the provided employee ID.
+    """
     if len(sys.argv) != 2:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
@@ -46,5 +84,4 @@ if __name__ == "__main__":
     print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
-
 
