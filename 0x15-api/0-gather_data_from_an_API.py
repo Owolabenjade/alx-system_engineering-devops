d1edff7058b85ceba22e53b4886afb31205b7cbe
Benjamin <owolabenjade@gmail.com>
2024-05-22 08:18:08 -0700

    baby

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,13 +16,36 @@ def get_data_from_api(uid):
         None
     """
     base = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(base + "users/" + uid).json()
-    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
-    completed = [_.get("title") for _ in userTodos if _.get("completed")]
+    
+    # Fetch user data
+    user_response = requests.get(base + "users/" + str(uid))
+    if user_response.status_code != 200:
+        print("Failed to retrieve user data")
+        return
+    user = user_response.json()
+    
+    # Fetch todos data
+    todos_response = requests.get(base + "todos", params={"userId": uid})
+    if todos_response.status_code != 200:
+        print("Failed to retrieve todos data")
+        return
+    userTodos = todos_response.json()
+    
+    completed = [todo.get("title") for todo in userTodos if todo.get("completed")]
     output = "Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(userTodos))
-    print("\n\t ".join([output] + completed))
+    
+    # Ensure proper formatting
+    task_output = "\n\t ".join([output] + completed)
+    print(task_output)
 
 
 if __name__ == "__main__":
-    get_data_from_api(sys.argv[1])
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            get_data_from_api(employee_id)
+        except ValueError:
+            print("Employee ID must be an integer.")
