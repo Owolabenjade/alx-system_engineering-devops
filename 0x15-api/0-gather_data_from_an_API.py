fd2134cdb1e3338d7a600eb33f5c25e973eb16bb
Benjamin <owolabenjade@gmail.com>
2024-05-22 08:22:07 -0700

    tried

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,25 +16,26 @@ def get_data_from_api(uid):
         None
     """
     base = "https://jsonplaceholder.typicode.com/"
-    
+
     # Fetch user data
     user_response = requests.get(base + "users/" + str(uid))
     if user_response.status_code != 200:
         print("Failed to retrieve user data")
         return
     user = user_response.json()
-    
+
     # Fetch todos data
     todos_response = requests.get(base + "todos", params={"userId": uid})
     if todos_response.status_code != 200:
         print("Failed to retrieve todos data")
         return
     userTodos = todos_response.json()
-    
-    completed = [todo.get("title") for todo in userTodos if todo.get("completed")]
+
+    completed = [todo.get("title")
+            for todo in userTodos if todo.get("completed")]
     output = "Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(userTodos))
-    
+
     # Ensure proper formatting
     task_output = "\n\t ".join([output] + completed)
     print(task_output)
