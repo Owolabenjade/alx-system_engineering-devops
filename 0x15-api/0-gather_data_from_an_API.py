44626feeeb6e1809e30d6383c64f6c8b7f2302f4
Benjamin <owolabenjade@gmail.com>
2024-05-22 08:04:40 -0700

    okay

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,6 @@ for a given employee ID, returns information about his/her TODO list progress
 import requests
 import sys
 
-
 def get_data_from_api(uid):
     """
     Gets and prints data from JSON PLACEHOLDER API
@@ -19,10 +18,6 @@ def get_data_from_api(uid):
     user = requests.get(base + "users/" + uid).json()
     userTodos = requests.get(base + "todos", params={"userId": uid}).json()
     completed = [_.get("title") for _ in userTodos if _.get("completed")]
-    output = "Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(userTodos))
-    print("\n\t ".join([output] + completed))
-
-
-if __name__ == "__main__":
-    get_data_from_api(sys.argv[1])
+    output = "Employee {:<18} is done with tasks({:<16}):".format(user.get("name"), "{}/{}".format(len(completed), len(userTodos)))
+    print("\\n{:<26}".format(output))
+    print("\n".join(completed))
