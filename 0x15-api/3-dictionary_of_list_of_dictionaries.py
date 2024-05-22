5681cee7902d48a04dc555486401d784af1c3ce5
Benjamin <owolabenjade@gmail.com>
2024-05-22 07:54:29 -0700

    oya

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,106 +1,33 @@
 #!/usr/bin/python3
 """
-This module fetches and exports the TODO list data for all employees from a
-REST API to a JSON file.
-
-Usage:
-    python3 3-dictionary_of_list_of_dictionaries.py
-
-The JSON file format is:
-{
-    "USER_ID": [
-        {
-            "username": "USERNAME",
-            "task": "TASK_TITLE",
-            "completed": TASK_COMPLETED_STATUS
-        },
-        ...
-    ],
-    ...
-}
-
-The output JSON file will be named 'todo_all_employees.json'.
+Python script that, using the JSON PLACEHOLDER API,
+for all employees, export data in the JSON format.
 """
-
 import json
 import requests
 
 
-def fetch_all_employees():
+def export_all_to_json():
     """
-    Fetches data for all employees from the API.
-
-    Returns:
-        list: A list of dictionaries containing employee data if the request is
-        successful.
-        None: If the request is unsuccessful.
+    Gets data from JSON PLACEHOLDER API and writes into json format
+    Return:
+        None
     """
-    url = 'https://jsonplaceholder.typicode.com/users'
-    response = requests.get(url)
-    if response.status_code != 200:
-        return None
-    return response.json()
-
-
-def fetch_todos():
-    """
-    Fetches TODO list data for all employees from the API.
-
-    Returns:
-        list: A list of dictionaries containing TODO list data if the request
-        is successful.
-        None: If the request is unsuccessful.
-    """
-    url = 'https://jsonplaceholder.typicode.com/todos'
-    response = requests.get(url)
-    if response.status_code != 200:
-        return None
-    return response.json()
-
-
-def export_to_json(data):
-    """
-    Exports the TODO list data to a JSON file.
-
-    Args:
-        data (dict): A dictionary containing the TODO list data for all employees.
-    """
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
+    base = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base + "users").json()
+    data = {}
+    for user in users:
+        _data = []
+        userTodos = requests.get(
+            base + "todos", params={"userId": user["id"]}).json()
+        for _ in userTodos:
+            row = {"username": user.get("username"),
+                   "task": _.get("title"), "completed": _.get("completed")}
+            _data.append(row)
+        data.update({user["id"]: _data})
+    with open("todo_all_employees.json", 'w', encoding="utf-8") as w:
+        w.write(json.dumps(data))
 
 
 if __name__ == "__main__":
-    """
-    Main entry point of the script. Fetches and exports the TODO list data for
-    all employees to a JSON file.
-    """
-    # Fetch all employee data
-    employees = fetch_all_employees()
-    if not employees:
-        print("Could not fetch employee data.")
-        sys.exit(1)
-
-    # Fetch all TODO list data
-    todos = fetch_todos()
-    if not todos:
-        print("Could not fetch TODO list data.")
-        sys.exit(1)
-
-    # Organize data
-    all_data = {}
-    for employee in employees:
-        employee_id = employee.get('id')
-        username = employee.get('username')
-        user_todos = [
-            {
-                "username": username,
-                "task": todo.get('title'),
-                "completed": todo.get('completed')
-            }
-            for todo in todos if todo.get('userId') == employee_id
-        ]
-        all_data[str(employee_id)] = user_todos
-
-    # Export to JSON
-    export_to_json(all_data)
+    export_all_to_json()
