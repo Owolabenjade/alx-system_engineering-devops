9c5a795f6833430b90dfa78f818c52642cc658e3
Benjamin <owolabenjade@gmail.com>
2024-07-02 18:09:58 -0700

    only

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,16 @@ Reddit API query to get the number of subscribers for a given subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
     If the subreddit is invalid, it returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my-user-agent'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
@@ -29,4 +31,3 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
