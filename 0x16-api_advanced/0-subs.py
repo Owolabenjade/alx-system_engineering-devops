ea07b41e14734f36a7f1b3fae343821c35bd6597
Benjamin <owolabenjade@gmail.com>
2024-07-02 18:17:48 -0700

    gbogbo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
+    Queries the Reddit API and returns the number
+    of subscribers for a given subreddit.
     If the subreddit is invalid, it returns 0.
     """
+    if len(subreddit) < 2:
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my-user-agent'}
 
@@ -20,8 +23,6 @@ def number_of_subscribers(subreddit):
         if response.status_code == 200:
             data = response.json()
             return data['data'].get('subscribers', 0)
-        elif response.status_code == 404:
-            return 0
         else:
             return 0
     except requests.RequestException:
