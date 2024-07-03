8be08ca65088e42f63e429a9b084b1aaaee49d4a
Benjamin <owolabenjade@gmail.com>
2024-07-02 18:33:35 -0700

    gbeni

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and return number of subscribers for a subreddit."""
+"""Query the Reddit API & return no. of subscribers for a subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Query the Reddit API and return the number of subscribers for a given subreddit.
-    
+    Query the Reddit API and return the number
+    of subscribers for a given subreddit.
+
     :param subreddit: string, the name of the subreddit to query
     :return: int, the number of subscribers (0 if invalid subreddit)
     """
     # Set up the API endpoint
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
+
     # Set a custom User-Agent to avoid Too Many Requests errors
     headers = {
         "User-Agent": "MyBot/1.0 (by /u/YourRedditUsername)"
     }
-    
+
     try:
         # Send GET request to the API
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+ 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             data = response.json()
