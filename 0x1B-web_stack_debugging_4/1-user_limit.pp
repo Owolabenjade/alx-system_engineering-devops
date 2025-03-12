c54f47a5164d531f8b56fd06df530ee42fa0ff67
Benjamin <obeejay99@gmail.com>
2025-03-12 01:53:38 +0100

    sixth

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,7 @@
 # Fixes "Too many open files" error
 
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "/holberton hard/c\holberton hard nofile 50000" /etc/security/limits.conf && sed -i "/holberton soft/c\holberton soft nofile 50000" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/c\holberton hard nofile 50000" /etc/security/limits.conf && \
+sed -i "/holberton soft/c\holberton soft nofile 50000" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/:/usr/bin/'
 }
