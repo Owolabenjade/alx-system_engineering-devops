8a814b126eabbb95443d87397d8a93105adfba45
Benjamin <obeejay99@gmail.com>
2025-03-12 01:42:01 +0100

    third

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Increases hard and soft file limits for the holberton user
+# Fixes "Too many open files" error
+
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'sed -i "/holberton hard/c\holberton hard nofile 50000" /etc/security/limits.conf && sed -i "/holberton soft/c\holberton soft nofile 50000" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/'
+}
