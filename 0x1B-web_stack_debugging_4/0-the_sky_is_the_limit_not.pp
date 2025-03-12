891b969a6b9edaaf5cb7e0e5ea187dfbbb5be6a6
Benjamin <obeejay99@gmail.com>
2025-03-12 01:44:31 +0100

    fourth

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Fixes Nginx to handle high loads of concurrent requests
-# Increases the ULIMIT for the Nginx process
+# Increases the ULIMIT and adjusts Nginx worker settings
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sed -i "s/worker_processes 4;/worker_processes auto;/g" /etc/nginx/nginx.conf && sed -i "/worker_connections/c\\worker_connections 4096;" /etc/nginx/nginx.conf && service nginx restart',
   path    => '/usr/local/bin/:/bin/:/usr/bin/'
 }
